4bc87e251d5dc257dc50960dca9e158bd6ce264c
Samuel <samadeolu7@gmail.com>
2025-02-11 17:17:06 +0100

    Fix: Working around the render.yaml

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Replace env vars in the template to generate the final config.
-envsubst '$BACKEND_HOST $BACKEND_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# (Removed uvicorn startup because backend service handles that)
-# Start Nginx in the foreground.
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Start Nginx
 nginx -g 'daemon off;'
