6a80a44e6063d8397b36e5cdc5de30251b6a9398
Samuel <samadeolu7@gmail.com>
2025-02-12 10:09:22 +0100

    try to debug env parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-envsubst '$BACKEND_HOST $BACKEND_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Print environment variables for debugging
+echo "BACKEND_HOST: $BACKEND_HOST"
+echo "BACKEND_PORT: $BACKEND_PORT"
+
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Print the generated Nginx configuration for debugging
+cat /etc/nginx/nginx.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
