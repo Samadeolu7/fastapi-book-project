9bbe8218fd0dde8c2ae21a892adb33ec8ad5b29c
Samuel <samadeolu7@gmail.com>
2025-02-12 09:25:32 +0100

    Fix: fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$BACKEND_HOST $BACKEND_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
